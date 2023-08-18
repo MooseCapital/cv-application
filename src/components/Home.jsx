ddb36086163dc5e88777efade273671a689cebb6
Moose Capital <48196495+MooseCapital@users.noreply.github.com>
2023-08-18 15:30:57 -0500

    save

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,8 +9,10 @@ function Home(props) {
         phone: "",
         schoolName: "",
         schoolStudy: "",
+        schoolGrad: "",
         employerName: "",
-
+        workPosition: "",
+        yearsWorked: "",
     })
 
     function handleChange(event) {
@@ -41,7 +43,9 @@ function Home(props) {
                         <label htmlFor="Name">Name:</label>
                         <input type="text" id={"Name"} placeholder={""} name={"name"} value={formData.name} onChange={handleChange}/>
                         <label htmlFor="Email">Email:</label>
-                        <input type="text" id={"Email"} placeholder={""} name={"email"} value={formData.email} onChange={handleChange}/>
+                        <input type="email" id={"Email"} placeholder={""} name={"email"} value={formData.email} onChange={handleChange}/>
+                        <label htmlFor="Phone">Phone:</label>
+                        <input type="text" id={"Phone"} placeholder={""} name={"phone"} value={formData.phone} onChange={handleChange}/>
 
 
                     </div>
@@ -49,12 +53,23 @@ function Home(props) {
                         <h5>School</h5>
                         <label htmlFor="schoolName">School Name:</label>
                         <input type="text" id={"text"} placeholder={""} name={"schoolName"} value={formData.schoolName} onChange={handleChange}/>
+                        <label htmlFor="schoolStudy">Degree:</label>
+                        <input type="text" id={"schoolStudy"} placeholder={""} name={"schoolStudy"} value={formData.schoolStudy} onChange={handleChange}/>
+                        <label htmlFor="schoolGrad">Year done:</label>
+                        <input type="text" id={"schoolGrad"} placeholder={""} name={"schoolGrad"} value={formData.schoolGrad} onChange={handleChange}/>
+            
 
                     </div>
                     <div className="exp-side">
                         <h5>Experience</h5>
                         <label htmlFor="employerName">Employer:</label>
                         <input type="text" id={"employerName"} placeholder={""} name={"employerName"} value={formData.employerName} onChange={handleChange}/>
+                        <label htmlFor="workPosition">position:</label>
+                        <input type="text" id={"workPosition"} placeholder={""} name={"workPosition"} value={formData.workPosition} onChange={handleChange}/>
+                        <label htmlFor="yearsWorked">Years worked:</label>
+
+                        <input type="text" id={"yearsWorked"} placeholder={""} name={"yearsWorked"} value={formData.yearsWorked} onChange={handleChange}/>
+                        
                     </div>
                     <div className="bottom-side">
                         <button>Edit</button>
